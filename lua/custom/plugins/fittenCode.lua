33b4f90a0b310da58fd81d3bb8538335f996c554
hhl from 303 <851315514@qq.com>
2024-10-11 10:25:55 +0800

    Add input auto-switch; Modify fittenCode

--- lua/custom/plugins/fittenCode.lua
+++ lua/custom/plugins/fittenCode.lua
@@ -1,26 +1,29 @@
 return {
   'luozhiya/fittencode.nvim',
   config = function()
-    require('fittencode').setup()
+    require('fittencode').setup({
+      use_default_keymaps = true,
+      action = {
+        keymaps = {
+          inline = {
+            -- ['<C-Down>'] = 'accept_all_suggestions',
+            ['<C-Down>'] = 'accept_line',
+            ['<C-Right>'] = 'accept_word',
+            ['<C-[>'] = 'revoke_line',
+            ['<C-Left>'] = 'revoke_word',
+            ['<A-\\>'] = 'triggering_completion',
+          },
+          chat = {
+            ['q'] = 'close',
+            ['[c'] = 'goto_previous_conversation',
+            [']c'] = 'goto_next_conversation',
+            ['c'] = 'copy_conversation',
+            ['C'] = 'copy_all_conversations',
+            ['d'] = 'delete_conversation',
+            ['D'] = 'delete_all_conversations',
+          },
+        },
+      }
+    })
   end,
-  use_default_keymaps = false,
-  keymaps = {
-    inline = {
-      ['<C-Down>'] = 'accept_all_suggestions',
-      ['<C-]>'] = 'accept_line',
-      ['<C-Right>'] = 'accept_word',
-      ['<C-[>'] = 'revoke_line',
-      ['<C-Left>'] = 'revoke_word',
-      ['<A-\\>'] = 'triggering_completion',
-    },
-    chat = {
-      ['q'] = 'close',
-      ['[c'] = 'goto_previous_conversation',
-      [']c'] = 'goto_next_conversation',
-      ['c'] = 'copy_conversation',
-      ['C'] = 'copy_all_conversations',
-      ['d'] = 'delete_conversation',
-      ['D'] = 'delete_all_conversations',
-    },
-  },
 }
